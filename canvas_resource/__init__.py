4dd1d476efcd41c351290b8ac10e66d477786152
Muttsu <22158218+Muttsu@users.noreply.github.com>
2018-11-22 02:58:48 -0500

    some test with session and flask-login

--- canvas_resource/__init__.py
+++ canvas_resource/__init__.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, session
 
 app = Flask(__name__)
+app.secret_key = b'secret'
 
 import canvas_resource.logging
 
 
 @app.route('/')
 def greeting():
+    session['hidden'] = 'info'
     return 'Hello, World!'
 
 if __name__ == '__main__':
